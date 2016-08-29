338db785c7d05265a1fab1e12c82513658a1ba7b
Max <max.wassler@gmail.com>
2016-08-29 23:11:51 +0200

    add body test + espacement

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ var bodyParser = require('koa-bodyparser');
 
 
 var app = koa();
+const port = '8080';
+
 app.use(bodyParser());
+
+app.use(route.get('/mail', function* mail(){
+	// ? here or not ? work _/
+	this.body = 'test';
+}));
+
+app.listen(port);
+console.log(port);
